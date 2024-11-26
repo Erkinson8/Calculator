47e184b0a502528821c8050cadd9494cd69c7e92
Erkinson8 <tunukerkin08@gmail.com>
2024-11-26 19:46:45 +0600

    hw_6

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.calculator;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -12,10 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private String operation = "";
-    private Double first;
-    private Double second;
+    private Double first, second, sum;
     private Boolean isOperationClick;
-
+    private MaterialButton sendButton;
 
 
     @Override
@@ -24,19 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text0);
     }
+
+
     public void onNumberClick(View view) {
 
-
+        sendButton = findViewById(R.id.send);
         if (view instanceof MaterialButton) {
             String text = ((MaterialButton) view).getText().toString();
             if (text.equals("AC")) {
                 textView.setText("0");
+                sendButton.setVisibility(View.INVISIBLE);
                 first = 0.0;
                 second = 0.0;
             } else if (textView.getText().toString().equals("0") || isOperationClick) {
                 textView.setText(text);
+                sendButton.setVisibility(View.INVISIBLE);
             } else if (textView.getText().toString().contains(".0")) {
                 textView.append(".");
+                sendButton.setVisibility(View.INVISIBLE);
 
             } else {
                 textView.append(text);
@@ -47,33 +52,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onOperationClick(View view) {
+        sendButton = findViewById(R.id.send);
         String text = ((MaterialButton) view).getText().toString();
-        Double sum;
         switch (text) {
             case "+":
                 first = Double.parseDouble(textView.getText().toString());
                 operation = "+";
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
             case "-":
                 first = Double.parseDouble(textView.getText().toString());
                 operation = "-";
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
             case "/":
                 first = Double.parseDouble(textView.getText().toString());
                 operation = "/";
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
             case ".":
                 first = Double.parseDouble(textView.getText().toString());
                 operation = ".";
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
             case "x":
                 first = Double.parseDouble(textView.getText().toString());
                 operation = "x";
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE); // Скрываем кнопку "Result"
                 break;
 
             case "%":
@@ -83,57 +93,64 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText(cancelDouble(value));
                     isOperationClick = true;
                 }
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
 
-            case "C":
-                String currentText = textView.getText().toString();
-                if (!currentText.isEmpty()) {
-                    currentText = currentText.substring(0, currentText.length() - 1);
-                }
-                if (currentText.isEmpty()) {
-                    currentText = "0";
-                }
-                textView.setText(currentText);
+            case "+/-":
+                double value = Double.parseDouble(textView.getText().toString());
+                value *= -1;
+                textView.setText(cancelDouble(value));
                 isOperationClick = true;
+                sendButton.setVisibility(View.INVISIBLE);
                 break;
-
 
 
             case "=":
 
                 second = Double.parseDouble(textView.getText().toString());
-                switch (operation) {
-                    case "+":
-                    case ".":
-                        sum = first + second;
-                        textView.setText(cancelDouble(sum));
-                        break;
-                    case "-":
-                        sum = first - second;
-                        textView.setText(cancelDouble(sum));
-                        break;
-                    case "/":
-                        sum = first / second;
-                        textView.setText(cancelDouble(sum));
-                        break;
-                    case "x":
-                        sum = first * second;
-                        textView.setText(cancelDouble(sum));
-                        break;
+                if (operation.equals("+")) {
+                    sum = first + second;
+                    textView.setText(cancelDouble(sum));
+                    sendButton.setVisibility(View.VISIBLE);
+                } else if (operation.equals("-")) {
+                    sum = first - second;
+                    textView.setText(cancelDouble(sum));
+                    sendButton.setVisibility(View.VISIBLE);
+                } else if (operation.equals("/")) {
+                    sum = first / second;
+                    textView.setText(cancelDouble(sum));
+                    sendButton.setVisibility(View.VISIBLE);
+                } else if (operation.equals("x")) {
+                    sum = first * second;
+                    textView.setText(cancelDouble(sum));
+                    sendButton.setVisibility(View.VISIBLE);
+                } else if (operation.equals(".")) {
+                    sum = first + second;
+                    textView.setText(cancelDouble(sum));
+                    sendButton.setVisibility(View.VISIBLE);
 
                 }
 
                 isOperationClick = true;
                 break;
+
+
         }
     }
 
     private String cancelDouble(Double number) {
         String text = number.toString();
-        if (text.endsWith(".0")) {
+        if (text.substring(text.length() - 2).equals(".0")) {
             return text.substring(0, text.length() - 2);
         } else {
             return number.toString();
         }
     }
+
+    public void onSendClick(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("result", textView.getText().toString());
+        startActivity(intent);
+    }
+
 }
